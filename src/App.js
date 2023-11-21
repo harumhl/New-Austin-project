ba462090fddf6a64c98b7d685887efce46146870
Haru Lee <haru.lee@nmrk.com>
2023-11-21 10:56:07 -0600

    links to google forms

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ function App() {
         <h1 style={{ paddingTop: "10px", paddingBottom: "10px" }}>
           "New Austin" project
         </h1>
+        <a
+          href="https://forms.gle/WjkCCYpsVcqG6BkGA"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Link to share comments
+        </a>
+        <br />
+        <a
+          href="https://forms.gle/91RtqrzZy6jByryB7"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Link to sign up for more
+        </a>
         <Overview />
         <div style={{ paddingTop: "30px" }}></div>
       </Header>
@@ -21,7 +36,8 @@ function App() {
       <Footer style={{ bottom: 0 }}>
         Context: I plan on building this city once I win $100 million on lottery
         (or any other means). "New Austin" is an arbitrary choice of the name
-        for the city and may change (though I believe it conveys the message well).
+        for the city and may change (though I believe it conveys the message
+        well).
       </Footer>
     </Container>
   );
