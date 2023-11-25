e6253cd3d37cc57883a0099ce2159d7a6b797389
Haru Lee <haru.lee@nmrk.com>
2023-11-24 20:55:49 -0600

    full width of images on mobile

--- src/texts/v1/images.js
+++ src/texts/v1/images.js
@@ -1,14 +1,18 @@
+import "./images.css";
+
 function Images() {
   return (
     <>
       <img
         src="https://raw.githubusercontent.com/harumhl/New-Austin-project/main/src/assets/inner_city.png"
         alt="inner_city.png"
+        className="full-width-on-mobile"
       ></img>
       <br />
       <img
         src="https://raw.githubusercontent.com/harumhl/New-Austin-project/main/src/assets/city_to_city.png"
         alt="city_to_city.png"
+        className="full-width-on-mobile"
       ></img>
     </>
   );
